9d9d8188b1ab9d349e02f8e1afc2c4d3e56d09c9
Angel Lakra <angel.lakra@chainlabs.in>
2024-11-12 03:10:09 +0530

    added skeleton to marketplace

--- src/app/(web3)/marketplace/components/TicketMarketplace.tsx
+++ src/app/(web3)/marketplace/components/TicketMarketplace.tsx
@@ -4,22 +4,26 @@ import React from "react";
 
 import useMarketplaceData from "../hooks/useMarketplaceData";
 
-import { TicketCard } from "./TicketCard";
+import { TicketCard, TicketsSkeleton } from "./TicketCard";
 
 export default function TicketMarketplace() {
-  const { marketTickets } = useMarketplaceData();
+  const { marketTickets, isLoading } = useMarketplaceData();
 
   return (
     <div className="mt-8">
       <h2 className="text-6xl font-bold mt-8 mb-4">Marketplace</h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {marketTickets.map(ticket => (
-          <TicketCard
-            key={`${ticket.tokenId}-${ticket.deadline}`}
-            ticket={ticket}
-          />
-        ))}
-      </div>
+      {isLoading ? (
+        <TicketsSkeleton />
+      ) : (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          {marketTickets.map(ticket => (
+            <TicketCard
+              key={`${ticket.tokenId}-${ticket.deadline}`}
+              ticket={ticket}
+            />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
--- src/app/(web3)/marketplace/hooks/useMarketplaceData.ts
+++ src/app/(web3)/marketplace/hooks/useMarketplaceData.ts
@@ -9,67 +9,46 @@ import { ITicketListed } from "../types";
 
 const useMarketplaceData = () => {
   const account = useAccount();
+  const [isLoading, setIsLoading] = useState(true);
   const [marketTickets, setMarketTickets] = useState<ITicketListed[]>([]);
 
   useEffect(() => {
-    if (account.address) {
-      const getAllTickets = async () => {
-        try {
-          const responseWithUser = await axios.post(envVars.subgraphUrl, {
-            query: getMarketplaceTicketsListWithUser,
-            variables: { id: `user-${account.address}` },
-          });
-          const listings = responseWithUser.data.data.listings?.items ?? [];
-          // const mergedTickets = Array.from(
-          //   new Set([
-          //     ...listings.map(
-          //       (ticket: { ticketId: string }) => ticket.ticketId
-          //     ),
-          //   ])
-          // );
-          // const ticketDetailsResponse = await axios.post(envVars.subgraphUrl, {
-          //   query: getTicketsFromAList,
-          //   variables: { id_in: mergedTickets },
-          // });
-          // const tickets: any = {};
-          // ticketDetailsResponse.data.data.tickets.items.forEach(
-          //   (ticket: any) => {
-          //     tickets[ticket.id] = {
-          //       seat: ticket.seat,
-          //       ticketSerialNumberHash: ticket.ticketSerialNumberHash,
-          //       owner: ticket.ownerId.split("-")[1],
-          //       event: ticket.event,
-          //     };
-          //   }
-          // );
-          const marketTickets: ITicketListed[] = listings.map((item: any) => {
-            console.log({ item });
-            return {
-              event: {
-                name: item?.ticket?.event?.name,
-                date: item?.ticket?.event?.eventDate,
-                image: item?.ticket?.tokenMetadata?.image,
-              },
-              owner: item?.owner,
-              seat: item?.ticket.seat,
-              ticketSerialNumberHash: item?.ticket.ticketSerialNumberHash,
-              tokenId: item.ticketId.split("-")[2],
-              listed: true,
-              price: item.price,
-              deadline: item.deadline,
-            };
-          });
-          setMarketTickets(marketTickets.filter(ticket => ticket !== null));
-        } catch (err) {
-          console.log(err);
-          return [];
-        }
-      };
-      getAllTickets();
-    }
-  }, [account]);
+    const getAllTickets = async () => {
+      try {
+        const responseWithUser = await axios.post(envVars.subgraphUrl, {
+          query: getMarketplaceTicketsListWithUser,
+          variables: { id: `user-${account.address}` },
+        });
+        const listings = responseWithUser.data.data.listings?.items ?? [];
+        const marketTickets: ITicketListed[] = listings.map((item: any) => {
+          console.log({ item });
+          return {
+            event: {
+              name: item?.ticket?.event?.name,
+              date: item?.ticket?.event?.eventDate,
+              image: item?.ticket?.tokenMetadata?.image,
+            },
+            owner: item?.owner,
+            seat: item?.ticket.seat,
+            ticketSerialNumberHash: item?.ticket.ticketSerialNumberHash,
+            tokenId: item.ticketId.split("-")[2],
+            listed: true,
+            price: item.price,
+            deadline: item.deadline,
+          };
+        });
+        setMarketTickets(marketTickets.filter(ticket => ticket !== null));
+      } catch (err) {
+        console.log(err);
+        return [];
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    getAllTickets();
+  }, []);
 
-  return { marketTickets };
+  return { marketTickets, isLoading };
 };
 
 export default useMarketplaceData;
