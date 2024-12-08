c2fd710062307c139825ea1d61b56ece0b6f12c7
Angel Lakra <angel.lakra@chainlabs.in>
2024-12-08 16:37:47 +0530

    size of modal fix on small screen

--- src/app/(web3)/marketplace/components/ListButton/Disclaimer.tsx
+++ src/app/(web3)/marketplace/components/ListButton/Disclaimer.tsx
@@ -18,46 +18,40 @@ const Disclaimer: React.FC<DisclaimerProps> = ({
   };
 
   return (
-    <div className="space-y-6">
-      <h5 className="text-md font-normal text-brandWhite">
-        <span>
-          Are you sure you want to list this ticket with the following details?
-        </span>
-      </h5>
-      <div className="bg-brandBlack px-8 py-6 mt-4 overflow-hidden border border-white border-opacity-15 rounded-[8px]">
-        <div className="flex p-6 text-sm items-center -rotate-12 scale-110 bg-brandBlue rounded-md shadow-md bg-opacity-50 text-white font-switzer gap-x-4 relative before:content-[''] before:absolute before:left-0 before:w-4 before:h-8 before:bg-black before:rounded-r-full before:top-1/2 before:-translate-y-1/2 after:content-[''] after:absolute after:w-4 after:h-8 after:bg-black after:rounded-l-full after:right-0 after:top-1/2 after:-translate-y-1/2">
-          <div>
+    <div className="space-y-6 max-h-[98vh] overflow-auto">
+      <div className="bg-[#1a1a1a] px-8 py-6 overflow-hidden border border-white/10 rounded-xl shadow-lg transition-all duration-300 hover:border-white/20">
+        <div
+          className="flex px-6 py-4 items-center -rotate-12 scale-110 hover:-rotate-3 transition-transform duration-300 bg-brandBlue/40 backdrop-blur-sm rounded-lg shadow-xl text-white font-switzer gap-x-6 relative
+        before:content-[''] before:absolute before:left-0 before:w-4 before:h-8 before:bg-[#1a1a1a] before:rounded-r-full before:top-1/2 before:-translate-y-1/2
+        after:content-[''] after:absolute after:w-4 after:h-8 after:bg-[#1a1a1a] after:rounded-l-full after:right-0 after:top-1/2 after:-translate-y-1/2"
+        >
+          <div className="relative w-[150px] h-[150px] rounded-lg overflow-hidden">
             <Image
               src={EVENTS.tbw.image}
               alt="event-image"
-              width={200}
-              height={200}
+              fill
+              className="object-cover"
             />
           </div>
-          <div className="flex flex-col gap-y-2 ">
-            <h5 className="text-slate-400 text-opacity-80">
-              Seat
-              <br />
-              <p className="font-bold text-xl text-white">{formData.seat}</p>
-            </h5>
-            <h5 className="text-slate-400 text-opacity-80">
-              TBW Moongate Token ID
-              <br />
-              <p className="font-bold text-xl text-white">
-                {formData.serialNumber}
-              </p>
-            </h5>
-            <h5 className="text-slate-400 text-opacity-80">
-              Price
-              <br />
-              <p className="font-bold text-xl text-white">
-                {formData.price} USD
-              </p>
-            </h5>
+          <div className="flex flex-col gap-y-2">
+            {[
+              { label: "Seat", value: formData.seat },
+              { label: "TBW Moongate Token ID", value: formData.serialNumber },
+              { label: "Price", value: `${formData.price} USD` },
+            ].map(({ label, value }) => (
+              <div key={label}>
+                <h5 className="text-slate-400/80 md:text-sm text-xs">
+                  {label}
+                </h5>
+                <p className="font-bold md:text-xl text-white text-sm">
+                  {value}
+                </p>
+              </div>
+            ))}
           </div>
         </div>
       </div>
-      <div className="mt-6 p-4 bg-yellow-200 rounded-md shadow-md bg-opacity-40 text-white border border-yellow-100">
+      <div className="mt-6 p-4 bg-yellow-200 rounded-md shadow-md bg-opacity-40 text-white border border-yellow-100 max-h-[250px] overflow-auto">
         <h5 className="text-lg underline mb-4 text-red-200 font-bold font-switzer">
           Please read the instructions below before you proceed:
         </h5>
